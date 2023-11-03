ffb8e898533b657ed5a0000d442ab6ef93bc83a8
Jaffar sadhiq <jaffarsdq007@gmail.com>
2023-11-03 21:21:18 +0530

    added tootip

--- src/components/BT_MiniCard.tsx
+++ src/components/BT_MiniCard.tsx
@@ -2,9 +2,11 @@
 
 function BT_MiniCard() {
   return (
-    <div className="flex flex-col justify-center border-[0.2px] border-slate-400 h-fit px-5 py-1 rounded-md text-green-500 font-normal m-1 uppercase">
-      <div className="mx-auto">10:54 AM</div>
-      <div className="">4k dolby 7.1</div>
+    <div className="tooltip" data-tip="hello">
+      <div className="flex flex-col justify-center border-[0.2px] border-slate-500/80 h-fit px-5 py-1 rounded-md text-red-500 font-normal m-1 uppercase cursor-default">
+        <div className="mx-auto">10:54 AM</div>
+        <div className="">4k dolby 7.1</div>
+      </div>
     </div>
   )
 }
